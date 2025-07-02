933c1f1b6034b2ef4ce0c1fc4d6449b34dcbf9cc
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-07-02 23:58:32 +0530

    hands on on the get and post operation in api using the postman app

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/")    ## here "get" is a method and "/" is a path where we want to go in our site
 def root():
         return {"massage": "Welcome to the my API!!!"}
 
 
+@app.get("/posts")
+def get_posts():
+        return {"data": "This is your posts"}
 
 
-
-
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+        print(payload)
+        return {"new_post": f"title {payload['title']} content: {payload['content']}"}
