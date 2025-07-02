696f55c387deb33eab2e3c6db15c4088c64e7f27
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-07-03 00:27:44 +0530

    update the curd operation

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import *
 
 app = FastAPI()
+
+class Post(BaseModel):
+        title: str
+        content: str
+        published: bool = True
+        rating: Optional[int] = None
+
+
 
 @app.get("/")    ## here "get" is a method and "/" is a path where we want to go in our site
 def root():
@@ -11,8 +22,22 @@ def root():
 def get_posts():
         return {"data": "This is your posts"}
 
+'''
+first method to fetch the data
 
 @app.post("/createposts")
 def create_posts(payload: dict = Body(...)):
         print(payload)
         return {"new_post": f"title {payload['title']} content: {payload['content']}"}
+
+'''
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+        print(new_post)
+        print(new_post.model_dump())   ## dict() ---> model_dump() 
+        return {"data": new_post}
+
+
+# title str, content str
+# end on 1:29:00 lecture start 1:29:00
